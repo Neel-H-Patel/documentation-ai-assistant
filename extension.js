2226521ae8eb844ee81241787e8f4cbbc1b8a9a6
maxlink <max.durham.link@gmail.com>
2024-08-08 14:50:45 -0500

    reenabled axios, rebuilt parsing
    reenabled axios to retrieve articles from https://blog.webdevsimplified.com/rss.xml
    
    rebuilt parsing to properly display label and detail when user searches.

--- extension.js
+++ extension.js
@@ -16,41 +16,39 @@ const { XMLParser } = require('fast-xml-parser');
  */
 async function activate(context) {
 
-	//const res = await axios.get("https://www.w3schools.com/xml/note.xml")
+	const res = await axios.get("https://blog.webdevsimplified.com/rss.xml")
 	//Replace the Axios request with a synchronous file read using fs.readFileSync. This reads the XML file into a string.
-	const filePath = path.join(__dirname, 'simple.xml'); // Path to your local XML file
-	const xmlData = fs.readFileSync(filePath, 'utf-8');  // Read the file content as a string
+	//const filePath = path.join(__dirname, 'simple.xml'); // Path to your local XML file
+	//const xmlData = fs.readFileSync(filePath, 'utf-8');  // Read the file content as a string
 
 	const parser = new XMLParser() 
 	//replace xmlData.data with res.data if using axios.get instead of lines 21-22
-	const parsed_res = parser.parse(xmlData)
+	const articles = parser.parse(res.data).rss.channel.item.map
+	(articles => {
+		return {
+		label: articles.title, 
+		detail: articles.description
+	}})
+	
 
-	// Adjusting to the actual structure of parsed_res
-    const lines = parsed_res.texts.line;
 
-	//console.log(parsed_res)
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	//console.log('Congratulations, your extension "customextension" is now active!');
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	// Mapping the lines to a format suitable for showQuickPick
-    const note = lines.map((line, index) => {
-        return {
-            label: `Line ${index + 1}`,  // Label each line with a simple index
-            detail: line,  // Use the text of the line as the detail
-        };
-    });
+   
 
 	const disposable = vscode.commands.registerCommand('customextension.searchWdsBlogExample', async function () {
-		// setting note equal to showQuickPick because as soon as someone picks a certain xml data, then it should be set equal to the result 
-		const chosen_note = await vscode.window.showQuickPick(note, { // creates a search bar like window for you to search through the xml data 
-			matchOnDetail: true //lets you match the title and the description of the xml data in the search bar 
+		//setting note equal to showQuickPick because as soon as someone picks a certain xml data, then it should be set equal to the result 
+		const chosen_article = await vscode.window.showQuickPick(articles, { // creates a search bar like window for you to search through the xml data 
+		matchOnDetail: true //lets you match the title and the description of the xml data in the search bar 
 		})
-		console.log("The User chose: ", chosen_note)
+		console.log("The User chose: ", chosen_article)
 	});
-
+	
 	context.subscriptions.push(disposable);
 }
 
